4707aec2a90c3eff220920a791b88645783165d2
Mojtaba Abedi <68202630+s-mojtaba-a@users.noreply.github.com>
2020-10-05 12:19:28 +0330

    :)

---
+++ Cpp.cpp
@@ -0,0 +1,12 @@
+#include <bits/stdc++.h>
+using namespace std;
+typedef long long ll;
+typedef pair<int, int> pii;
+#define all(v) (v).begin(),(v).end()
+
+
+int main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(0);
+}
