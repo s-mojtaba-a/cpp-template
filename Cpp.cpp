b638bbfb020e1f017091e69434ae6313fe758ef8
Mojtaba Abedi <68202630+s-mojtaba-a@users.noreply.github.com>
2020-10-12 20:11:13 +0330

    Update Cpp.cpp

--- Cpp.cpp
+++ Cpp.cpp
@@ -5,12 +5,10 @@ using namespace std;
 typedef long long ll;
 typedef pair<int, int> pii;
 typedef pair<ll, ll> pll;
-
-inline ll Mod(ll x, ll mod) return x % mod >= 0 ? x % mod : x % mod + mod;
+inline ll Mod(ll x, ll mod) { return x % mod >= 0 ? x % mod : x % mod + mod; }
 
 int main()
 {
     ios_base::sync_with_stdio(false);
     cin.tie(0);
-    cout << Mod(-19, 10);
 }
